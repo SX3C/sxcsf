9ee47db08a7a601ad3eadea0dcc576769aa48a71
crebro <creationduwal@gmail.com>
2024-09-06 07:23:52 +0545

    chore: add widget to appear after countdown ends

--- src/components/CountdownWidget.js
+++ src/components/CountdownWidget.js
@@ -2,29 +2,40 @@ import React from "react";
 import Countdown from "react-countdown";
 
 function renderer({ days, hours, minutes, seconds, completed }) {
-  if (completed) {
-    return <span className="text-4xl font-bold text-black">Event Started</span>;
-  }
-
   return (
-    <div className="flex items-center justify-center space-x-4">
-      <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold text-black">{days}</span>
-        <span className="text-sm text-gray-500">Days</span>
+    <>
+      <div className="flex items-center justify-center space-x-4">
+        <div className="flex flex-col items-center">
+          <span className="text-4xl font-bold text-black">
+            {completed ? 0 : days}
+          </span>
+          <span className="text-sm text-gray-500">Days</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-4xl font-bold text-black">
+            {completed ? 0 : hours}
+          </span>
+          <span className="text-sm text-gray-500">Hours</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-4xl font-bold text-black">
+            {completed ? 0 : minutes}
+          </span>
+          <span className="text-sm text-gray-500">Minutes</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-4xl font-bold text-black">
+            {completed ? 0 : seconds}
+          </span>
+          <span className="text-sm text-gray-500">Seconds</span>
+        </div>
       </div>
-      <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold text-black">{hours}</span>
-        <span className="text-sm text-gray-500">Hours</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold text-black">{minutes}</span>
-        <span className="text-sm text-gray-500">Minutes</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold text-black">{seconds}</span>
-        <span className="text-sm text-gray-500">Seconds</span>
-      </div>
-    </div>
+      {completed ? (
+        <div className="text-4xl font-bold text-black mt-4">It's Here!!!</div>
+      ) : (
+        <> </>
+      )}
+    </>
   );
 }
 
